270f205bceb7e3fde0e35c86325d30abc3b62242
Juan Rubio <juan.rubio@globant.com>
2019-07-14 23:51:46 -0500

    email_alerts

--- bridge.py
+++ bridge.py
@@ -1,9 +1,14 @@
 import mysql.connector
 from mysql.connector import Error
 from mysql.connector import errorcode
+import paho.mqtt.client as mqtt #import the client1
 import json
 import time
-import paho.mqtt.client as mqtt #import the client1
+
+import sqlite3
+import requests
+from sqlite3 import Error
+
 
 def on_message(client, userdata, message):
   payload = str(message.payload.decode("utf-8"))
@@ -12,25 +17,70 @@ def on_message(client, userdata, message):
   print("message qos=", message.qos)
   print("message retain flag=", message.retain)
   write_to_db(payload)
+  check_rules(payload)
+
+def send_mail(rule, send_to, point_name):
+  url = "https://api.sendinblue.com/v3/smtp/email"
+  to = []
+  for receiver in send_to:
+    to.append(
+      {
+        "email": receiver
+      }
+    )
+  sensed_value = eval(rule['metric'])
+  payload = {
+    "sender":{
+      "name":"no_reply_UNAL",
+      "email":"jcrubioa@unal.edu.co"
+    },
+    "to": to,
+    "textContent":"La variable \"{metric}\" ha salido del umbral establecido: \"{metric} {comparator} {value}\".\n Valor sensado: \"{sensed_value}\""\
+      .format(metric=rule['metric'], comparator=rule['comparator'], value=rule['value'], sensed_value=round(sensed_value, 2)),
+    "subject": "Alerta Preventiva: Valor sensado en el dispositivo \"{point_name}\" fuera de los umbrales establecidos".format(point_name=point_name)
+  }
+  #"replyTo":{"email":"jcrubioa2@gmail.com"}
+  headers = {
+      'accept': "application/json",
+      'content-type': "application/json",
+      'api-key': "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"
+      }
+  print(payload)
+  response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+  print(response.text)
+
+def check_rules(payload):
+  print('check_rules')
+  data = json.loads(payload)
+  for metric in data['metrics']:
+    exec('{} = {}'.format(metric['name'], metric['value']), globals())
+    print('{} = {}'.format(metric['name'], metric['value']))
+  with open('rules.json') as json_file:
+    rules_config = json.load(json_file)
+    device_id = data['device_id']
+    for rule in rules_config['rules'][device_id]['thresholds']:
+      rule_expression = "{} {} {}".format(rule['metric'], rule['comparator'], rule['value'])
+      triggered = eval(rule_expression)
+      if triggered:
+        send_to = rules_config['rules'][device_id]['emails']
+        point_name = rules_config['rules'][device_id]['point_name']
+        send_mail(rule, send_to, point_name)
+
 
 
 def write_to_db(payload):
   data = json.loads(payload)
   print("Payload: {}".format(data))
-
-  
   measure_query_schema = """ 
                         INSERT INTO `measure`
                         (measure_id, device_id, description, lat, lng, event_timestamp)
                         VALUES ('{}','{}', '{}', {}, {}, '{}')
                       """
-
   metric_query_schema = """ 
                         INSERT INTO `metric`
                         (measure_id, metric_name, metric_value)
                         VALUES ('{}','{}', {})
                       """
-
   try:
     measure_query = measure_query_schema.\
       format(data['measure_id'], data['device_id'], data['description'], data['lat'], data['lng'], data['event_timestamp'])
@@ -46,8 +96,6 @@ def write_to_db(payload):
   except mysql.connector.Error as error :
     connection.rollback() #rollback if any exception occured
     print("Failed inserting record into python_users table {}".format(error))
-
-  #cursor.close()
 
 
 
